f7bd7d3d3c015bff40fd731c182a9c77d3427176
kusi <appaukusi05@gmail.com>
2024-05-31 19:29:08 +0000

    added for loops to create the grids
    the first for loop created 16 rows of divs in which 16 other column
    divs were created.

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // Creating the grid squares inside the container div
 const gridContainer = document.querySelector(".container");
 
-function createDivs(num){
-    for(let i = 1; i <= num; i++){
-        const grid = document.createElement("div");
-        grid.className = "grid-square";
-        gridContainer.appendChild(grid);
+function createDiv(num){
+    for (let i=1; i <= num; i++){
+        const rowDiv = document.createElement("div");
+        rowDiv.className = "row";
+        gridContainer.appendChild(rowDiv);
     }
+    for (let i=1; i <= num; i++){
+        const rowList = document.querySelectorAll(".row")
+        const getRows = Array.from(rowList);
+        getRows.forEach(row => {
+            const colDiv = document.createElement("div");
+            colDiv.className = "col";
+            row.appendChild(colDiv);
+        });
+    }   
 }
-
-
-createDivs(16);
+createDiv(16);
